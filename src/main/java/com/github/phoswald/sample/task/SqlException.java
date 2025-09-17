9b21f9d166c5141ab0c9892d18196c2e363a9efd
Philip Oswald <philip.oswald@gmail.com>
2025-09-17 17:25:11 +0200

    Experiment with Java 25: constructor body

--- src/main/java/com/github/phoswald/sample/task/SqlException.java
+++ src/main/java/com/github/phoswald/sample/task/SqlException.java
@@ -5,10 +5,13 @@ public class SqlException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     SqlException(Throwable cause) {
-        super(cause);
+        this(null, cause);
     }
 
     SqlException(String message, Throwable cause) {
+        if(cause == null) {
+            throw new IllegalArgumentException("no cause given");
+        }
         super(message, cause);
     }
 }
